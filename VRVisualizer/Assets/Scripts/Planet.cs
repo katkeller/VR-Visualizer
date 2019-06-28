56daf928d5c302cc57ee39f0ef6fa45e63b0e99f
Kat Keller <kat.kek.keller@gmail.com>
2019-06-28 18:04:17 -0500

    Added working shooting to the gun that destroys the planet objects, then respawns them after a delay

---
+++ VRVisualizer/Assets/Scripts/Planet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField]
+    private float respawnDelay = 5.0f;
+
+    [SerializeField]
+    private AudioClip destroy, respawn;
+
+    private bool isDestroyed = false;
+    private MeshRenderer meshRenderer;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TakeDamage()
+    {
+        if (!isDestroyed)
+        {
+            audioSource.PlayOneShot(destroy, 0.1f);
+            meshRenderer.enabled = false;
+            isDestroyed = true;
+            StartCoroutine(DelayRespawn());
+        }
+    }
+
+    IEnumerator DelayRespawn()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        audioSource.PlayOneShot(respawn, 0.1f);
+        meshRenderer.enabled = true;
+        isDestroyed = false;
+    }
+}
--- VRVisualizer/Assets/Scripts/ShootGun.cs
+++ VRVisualizer/Assets/Scripts/ShootGun.cs
@@ -12,6 +12,13 @@ public class ShootGun : MonoBehaviour
     [SerializeField]
     private GameObject gun;
 
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
         if (SteamVR_Actions._default.Shoot.GetStateDown(SteamVR_Input_Sources.Any))
@@ -22,10 +29,20 @@ public class ShootGun : MonoBehaviour
 
     private void Shoot()
     {
+        audioSource.Play();
+
         RaycastHit hitInfo;
+
         if (Physics.Raycast(gun.transform.position, gun.transform.forward, out hitInfo, range))
         {
             Debug.Log("You hit " + hitInfo.transform.name);
+
+            Planet planet = hitInfo.transform.GetComponent<Planet>();
+
+            if (planet != null)
+            {
+                planet.TakeDamage();
+            }
         }
     }
 }
