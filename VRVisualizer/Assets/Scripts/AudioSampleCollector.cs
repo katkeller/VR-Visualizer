3ddb13c6c49d914a743d954c0e13dca1721af935
Kat Keller <kat.kek.keller@gmail.com>
2019-06-28 15:57:33 -0500

    Deleted unused scripts from asset packs, created working audio visualizer planets that rotate around the player and light up

--- VRVisualizer/Assets/Scripts/AudioSampleCollector.cs
+++ VRVisualizer/Assets/Scripts/AudioSampleCollector.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class AudioSampleCollector : MonoBehaviour
 {
     private AudioSource audioSource;
--- VRVisualizer/Assets/Scripts/LightVisualizer.cs
+++ VRVisualizer/Assets/Scripts/LightVisualizer.cs
@@ -9,50 +9,65 @@ public class LightVisualizer : MonoBehaviour
 
     [Tooltip("Enter a number between 0-7 to choose which of the 8 frequency bands will be sampled from the AudioSampleCollector script.")]
     [SerializeField]
-    private int frequencyBandToSample;
+    private int[] frequencyBandToSample = new int[8];
 
     [SerializeField]
     private bool useBuffer;
 
     [SerializeField]
-    private Light lightSource;
+    private Light[] lightSource = new Light[8];
+
+    //[SerializeField]
+    //private GameObject[] crystal = new GameObject[8];
 
     [SerializeField]
-    private GameObject[] crystal = new GameObject[8];
+    private float rotationsPerMinute = 8.0f;
 
-    private Material[] material = new Material[8];
+    //private Material[] material = new Material[8];
 
     private void Start()
     {
         //material = GetComponent<MeshRenderer>().materials[0];
-        for (int i = 0; i < 8; i++)
-        {
-            material[i] = crystal[i].GetComponent<MeshRenderer>().materials[0];
-        }
+        //for (int i = 0; i < 8; i++)
+        //{
+        //    material[i] = crystal[i].GetComponent<MeshRenderer>().materials[0];
+        //}
     }
 
     private void Update()
     {
         UpdateLightIntensity();
-        UpdateMaterial();
+        //UpdateMaterial();
+
+        transform.Rotate(Vector3.one * rotationsPerMinute * Time.deltaTime);
     }
 
-    private void UpdateMaterial()
-    {
-        if (useBuffer)
-        {
-            Color color = new Color(AudioSampleCollector.audioBandBuffer[frequencyBandToSample], AudioSampleCollector.audioBandBuffer[frequencyBandToSample], AudioSampleCollector.audioBandBuffer[frequencyBandToSample]);
-            material.SetColor("_EmissionColor", color);
-        }
-        else if (!useBuffer)
-        {
-            Color color = new Color(AudioSampleCollector.audioBand[frequencyBandToSample], AudioSampleCollector.audioBand[frequencyBandToSample], AudioSampleCollector.audioBand[frequencyBandToSample]);
-            material.SetColor("_EmissionColor", color);
-        }
-    }
+    //private void UpdateMaterial()
+    //{
+    //    if (useBuffer)
+    //    {
+    //        for (int e = 0; e < 8; e++)
+    //        {
+    //            Color color = new Color(AudioSampleCollector.audioBandBuffer[frequencyBandToSample[e]], AudioSampleCollector.audioBandBuffer[frequencyBandToSample[e]], AudioSampleCollector.audioBandBuffer[frequencyBandToSample[e]]);
+    //            material[e].SetColor("_EmissionColor", color);
+    //        }
+
+    //    }
+    //    else if (!useBuffer)
+    //    {
+    //        for (int j = 0; j < 8; j++)
+    //        {
+    //            Color color = new Color(AudioSampleCollector.audioBand[frequencyBandToSample[j]], AudioSampleCollector.audioBand[frequencyBandToSample[j]], AudioSampleCollector.audioBand[frequencyBandToSample[j]]);
+    //            material[j].SetColor("_EmissionColor", color);
+    //        }
+    //    }
+    //}
 
     private void UpdateLightIntensity()
     {
-        lightSource.intensity = (AudioSampleCollector.audioBandBuffer[frequencyBandToSample] * (maxLightIntensity - minLightIntensity)) + minLightIntensity;
+        for (int k = 0; k < 8; k++)
+        {
+            lightSource[k].intensity = (AudioSampleCollector.audioBandBuffer[frequencyBandToSample[k]] * (maxLightIntensity - minLightIntensity)) + minLightIntensity;
+        }
     }
 }
