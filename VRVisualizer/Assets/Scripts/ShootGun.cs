a59f8368f3907a040578005ee3b1fe0f3c3e4b7f
Kat Keller <kat.kek.keller@gmail.com>
2019-06-28 17:18:43 -0500

    Added gun model and added raycasting to the gun

---
+++ VRVisualizer/Assets/Scripts/ShootGun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ShootGun : MonoBehaviour
+{
+    [SerializeField]
+    private float range = 100f;
+
+    [SerializeField]
+    private GameObject gun;
+
+    private void Update()
+    {
+        if (SteamVR_Actions._default.Shoot.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(gun.transform.position, gun.transform.forward, out hitInfo, range))
+        {
+            Debug.Log("You hit " + hitInfo.transform.name);
+        }
+    }
+}
