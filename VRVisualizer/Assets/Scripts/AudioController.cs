7ad8da9167bf583113d993399b1b7a43b3a7f60b
Kat Keller <kat.kek.keller@gmail.com>
2019-06-29 15:44:21 -0500

    Added functionality that allows the player to switch between three songs while playing

---
+++ VRVisualizer/Assets/Scripts/AudioController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip clip1, clip2, clip3;
+
+    private AudioSource audioSource;
+    private bool clip1IsPlaying = false;
+    private bool clip2IsPlaying = false;
+    private bool clip3IsPlaying = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(clip1);
+        clip1IsPlaying = true;
+    }
+
+    private void Update()
+    {
+        if (SteamVR_Actions._default.AButtonPress.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            if (clip1IsPlaying)
+            {
+                audioSource.Stop();
+                clip1IsPlaying = false;
+                audioSource.PlayOneShot(clip2);
+                clip2IsPlaying = true;
+            }
+            else if (clip2IsPlaying)
+            {
+                audioSource.Stop();
+                clip2IsPlaying = false;
+                audioSource.PlayOneShot(clip3);
+                clip3IsPlaying = true;
+            }
+            else if (clip3IsPlaying)
+            {
+                audioSource.Stop();
+                clip3IsPlaying = false;
+                audioSource.PlayOneShot(clip1);
+                clip1IsPlaying = true;
+            }
+        }
+    }
+}
--- VRVisualizer/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VRVisualizer/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -97,6 +97,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean AButtonPress
+        {
+            get
+            {
+                return SteamVR_Actions.default_AButtonPress;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
